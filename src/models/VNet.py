7a8418a7a43a1536a7c8b8b467b640a0d3a5a981
Carlos Aumente <carlos.aumente@amac02ft4a3md6m.station>
2021-12-11 20:21:55 +0000

    VNet architectured refactored and tested

--- src/models/VNet.py
+++ src/models/VNet.py
@@ -2,18 +2,24 @@ import torch.nn as nn
 import torch
 
 """
-Implementation of this model is borrowed and modified
-(to support multi-channels and latest pytorch version)
-from here:
+Implementation of this model is borrowed and modified (to support multi-channels and latest pytorch version) from:
 https://github.com/Dawn90/V-Net.pytorch
 """
 
 
-def passthrough(x, **kwargs):
+def passthrough(x):
+    """
+    This layer is useful to not do anything action over the input
+    """
     return x
 
+def make_convs_residuals(n_channels, n_convolutions, elu):
+    layers = []
+    for _ in range(n_convolutions):
+        layers.append(LUConv(n_channels, elu))
+    return nn.Sequential(*layers)
 
-def ELUCons(elu, nchan):
+def ActivationLayer(elu, nchan):
     if elu:
         return nn.ELU(inplace=True)
     else:
@@ -21,23 +27,15 @@ def ELUCons(elu, nchan):
 
 
 class LUConv(nn.Module):
-    def __init__(self, nchan, elu):
+    def __init__(self, n_channels, elu):
         super(LUConv, self).__init__()
-        self.relu1 = ELUCons(elu, nchan)
-        self.conv1 = nn.Conv3d(nchan, nchan, kernel_size=5, padding=2)
-
-        self.bn1 = torch.nn.BatchNorm3d(nchan)
+        self.conv = nn.Conv3d(n_channels, n_channels, kernel_size=5, padding=2)
+        self.norm = torch.nn.BatchNorm3d(n_channels)
+        self.activation = ActivationLayer(elu, n_channels)
 
     def forward(self, x):
-        out = self.relu1(self.bn1(self.conv1(x)))
+        out = self.activation(self.norm(self.conv(x)))
         return out
-
-
-def _make_nConv(nchan, depth, elu):
-    layers = []
-    for _ in range(depth):
-        layers.append(LUConv(nchan, elu))
-    return nn.Sequential(*layers)
 
 
 class InputTransition(nn.Module):
@@ -46,39 +44,41 @@ class InputTransition(nn.Module):
         self.num_features = 16
         self.in_channels = in_channels
 
-        self.conv1 = nn.Conv3d(self.in_channels, self.num_features, kernel_size=5, padding=2)
-
-        self.bn1 = torch.nn.BatchNorm3d(self.num_features)
-
-        self.relu1 = ELUCons(elu, self.num_features)
+        self.conv1 = nn.Conv3d(in_channels=self.in_channels, out_channels=self.num_features, kernel_size=5, padding=2)
+        self.bn1 = torch.nn.BatchNorm3d(num_features=self.num_features)
+        self.relu1 = ActivationLayer(elu, self.num_features)
 
     def forward(self, x):
         out = self.conv1(x)
-        repeat_rate = int(self.num_features / self.in_channels)
         out = self.bn1(out)
-        x16 = x.repeat(1, repeat_rate, 1, 1, 1)
-        return self.relu1(torch.add(out, x16))
+
+        repeat_rate = int(self.num_features / self.in_channels)
+        x_residual = x.repeat(1, repeat_rate, 1, 1, 1)
+
+        return self.relu1(torch.add(out, x_residual))
 
 
 class DownTransition(nn.Module):
-    def __init__(self, inChans, nConvs, elu, dropout=False):
+    def __init__(self, in_channels, n_convolutions, elu, dropout=False):
         super(DownTransition, self).__init__()
-        outChans = 2 * inChans
-        self.down_conv = nn.Conv3d(inChans, outChans, kernel_size=2, stride=2)
-        self.bn1 = torch.nn.BatchNorm3d(outChans)
+        out_channels = 2 * in_channels
 
-        self.do1 = passthrough
-        self.relu1 = ELUCons(elu, outChans)
-        self.relu2 = ELUCons(elu, outChans)
+        self.down_conv = nn.Conv3d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.norm = torch.nn.BatchNorm3d(out_channels)
+        self.activation = ActivationLayer(elu, out_channels)
+
         if dropout:
             self.do1 = nn.Dropout3d()
-        self.ops = _make_nConv(outChans, nConvs, elu)
+        else:
+            self.do1 = passthrough
+
+        self.ops = make_convs_residuals(out_channels, n_convolutions, elu)
 
     def forward(self, x):
-        down = self.relu1(self.bn1(self.down_conv(x)))
+        down = self.activation(self.norm(self.down_conv(x)))
         out = self.do1(down)
         out = self.ops(out)
-        out = self.relu2(torch.add(out, down))
+        out = self.activation(torch.add(out, down))
         return out
 
 
@@ -88,21 +88,24 @@ class UpTransition(nn.Module):
         self.up_conv = nn.ConvTranspose3d(inChans, outChans // 2, kernel_size=2, stride=2)
 
         self.bn1 = torch.nn.BatchNorm3d(outChans // 2)
-        self.do1 = passthrough
         self.do2 = nn.Dropout3d()
-        self.relu1 = ELUCons(elu, outChans // 2)
-        self.relu2 = ELUCons(elu, outChans)
+        self.activation1 = ActivationLayer(elu, outChans // 2)
+        self.activation2 = ActivationLayer(elu, outChans)
+
         if dropout:
             self.do1 = nn.Dropout3d()
-        self.ops = _make_nConv(outChans, nConvs, elu)
+        else:
+            self.do1 = passthrough
+
+        self.ops = make_convs_residuals(outChans, nConvs, elu)
 
     def forward(self, x, skipx):
         out = self.do1(x)
         skipxdo = self.do2(skipx)
-        out = self.relu1(self.bn1(self.up_conv(out)))
+        out = self.activation1(self.bn1(self.up_conv(out)))
         xcat = torch.cat((out, skipxdo), 1)
         out = self.ops(xcat)
-        out = self.relu2(torch.add(out, xcat))
+        out = self.activation2(torch.add(out, xcat))
         return out
 
 
@@ -111,14 +114,14 @@ class OutputTransition(nn.Module):
         super(OutputTransition, self).__init__()
         self.classes = classes
         self.conv1 = nn.Conv3d(in_channels, classes, kernel_size=5, padding=2)
-        self.bn1 = torch.nn.BatchNorm3d(classes)
+        self.norm = torch.nn.BatchNorm3d(classes)
 
         self.conv2 = nn.Conv3d(classes, classes, kernel_size=1)
-        self.relu1 = ELUCons(elu, classes)
+        self.activation = ActivationLayer(elu, classes)
 
     def forward(self, x):
         # convolve 32 down to channels as the desired classes
-        out = self.relu1(self.bn1(self.conv1(x)))
+        out = self.activation(self.norm(self.conv1(x)))
         out = self.conv2(out)
         return out
 
@@ -127,14 +130,12 @@ class VNet(nn.Module):
     """
     Implementations based on the Vnet paper: https://arxiv.org/abs/1606.04797
     """
+    name = 'Vnet'
 
-    def __init__(self, elu=True, in_channels=1, classes=4):
+    def __init__(self, elu=True, sequences=4, regions=3):
         super(VNet, self).__init__()
-        self.classes = classes
-        self.in_channels = in_channels
-        self.deep_supervision = False
 
-        self.in_tr = InputTransition(in_channels, elu=elu)
+        self.in_tr = InputTransition(sequences, elu=elu)
         self.down_tr32 = DownTransition(16, 1, elu)
         self.down_tr64 = DownTransition(32, 2, elu)
         self.down_tr128 = DownTransition(64, 3, elu, dropout=True)
@@ -143,7 +144,7 @@ class VNet(nn.Module):
         self.up_tr128 = UpTransition(256, 128, 2, elu, dropout=True)
         self.up_tr64 = UpTransition(128, 64, 1, elu)
         self.up_tr32 = UpTransition(64, 32, 1, elu)
-        self.out_tr = OutputTransition(32, classes, elu)
+        self.out_tr = OutputTransition(32, regions, elu)
 
     def forward(self, x):
         out16 = self.in_tr(x)
@@ -156,59 +157,22 @@ class VNet(nn.Module):
         out = self.up_tr64(out, out32)
         out = self.up_tr32(out, out16)
         out = self.out_tr(out)
+
         return out
 
-    def test(self):
-        input_tensor = torch.rand(1, self.in_channels, 32, 32, 32)
-        ideal_out = torch.rand(1, self.classes, 32, 32, 32)
-        out = self.forward(input_tensor)
-        assert ideal_out.shape == out.shape
 
-        print("Vnet test is complete")
+def test():
+    seq_input = torch.rand(1, 4, 160, 224, 160)
+    seq_ouput = torch.rand(1, 3, 160, 224, 160)
 
+    model = VNet(elu=True, sequences=4, regions=3)
+    preds = model(seq_input)
 
-class VNetLight(nn.Module):
-    """
-    A lighter version of Vnet that skips down_tr256 and up_tr256 in order to reduce time and space complexity
-    """
+    print(seq_input.shape)
+    print(preds.shape)
 
-    def __init__(self, elu=True, in_channels=1, classes=4):
-        super(VNetLight, self).__init__()
-        self.classes = classes
-        self.in_channels = in_channels
-
-        self.in_tr = InputTransition(in_channels, elu)
-        self.down_tr32 = DownTransition(16, 1, elu)
-        self.down_tr64 = DownTransition(32, 2, elu)
-        self.down_tr128 = DownTransition(64, 3, elu, dropout=True)
-        self.up_tr128 = UpTransition(128, 128, 2, elu, dropout=True)
-        self.up_tr64 = UpTransition(128, 64, 1, elu)
-        self.up_tr32 = UpTransition(64, 32, 1, elu)
-        self.out_tr = OutputTransition(32, classes, elu)
-
-    def forward(self, x):
-        out16 = self.in_tr(x)
-        out32 = self.down_tr32(out16)
-        out64 = self.down_tr64(out32)
-        out128 = self.down_tr128(out64)
-        out = self.up_tr128(out128, out64)
-        out = self.up_tr64(out, out32)
-        out = self.up_tr32(out, out16)
-        out = self.out_tr(out)
-        return out
-
-    def test(self):
-        input_tensor = torch.rand(1, self.in_channels, 32, 32, 32)
-        ideal_out = torch.rand(1, self.classes, 32, 32, 32)
-        out = self.forward(input_tensor)
-        assert ideal_out.shape == out.shape
-
-        print("Vnet light test is complete")
+    assert seq_ouput.shape == preds.shape
 
 
-def get_Vnet(sequences=4, regions=3):
-    model = VNet(in_channels=sequences, classes=regions)
-    model.name = 'Vnet'
-    model.deep_supervision = False
-
-    return model
+if __name__ == "__main__":
+    test()
