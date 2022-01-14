1478b0ffca330863307627301f2db906ed96af21
caumente <carlosaumente@gmail.com>
2022-01-14 23:41:34 +0000

    Seed everything

--- src/utils/miscellany.py
+++ src/utils/miscellany.py
@@ -272,8 +272,9 @@ def seed_everything(seed: int):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    #torch.backends.cudnn.deterministic = True
-    #torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+    torch.cuda.manual_seed_all(seed)
 
 
 
