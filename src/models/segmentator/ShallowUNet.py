bcbce321a5fc5ab564db8f77baafb3653973e6c4
Carlos Aumente <carlosaumente@gmail.com>
2022-09-19 20:36:19 +0200

    Added a piece of code to size a model in MB

--- src/models/segmentator/ShallowUNet.py
+++ src/models/segmentator/ShallowUNet.py
@@ -40,7 +40,6 @@ class ShallowUNet(nn.Module):
         self.upsample = nn.Upsample(scale_factor=2, mode="trilinear", align_corners=True)
         self.downsample = nn.MaxPool3d(2, 2)
 
-
         # Output
         if self.deep_supervision:
             self.output3 = nn.Sequential(
@@ -98,8 +97,7 @@ class ShallowUNet(nn.Module):
             return output1
 
 
-
-def test():
+if __name__ == "__main__":
     seq_input = torch.rand(1, 4, 160, 224, 160)
     seq_ouput = torch.rand(1, 3, 160, 224, 160)
 
@@ -115,6 +113,12 @@ def test():
         print(preds.shape)
         assert seq_ouput.shape == preds.shape
 
+    param_size = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    buffer_size = 0
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
 
-if __name__ == "__main__":
-    test()
+    size_all_mb = (param_size + buffer_size) / 1024 ** 2
+    print('model size: {:.3f}MB'.format(size_all_mb))
