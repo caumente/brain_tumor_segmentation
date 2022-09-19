0acad94bd26ff1971ddab0e8b380e76f29f17c5e
Carlos Aumente <carlosaumente@gmail.com>
2022-09-19 20:31:54 +0200

    Modified 3DUNet model

--- src/models/segmentator/Unet3D.py
+++ src/models/segmentator/Unet3D.py
@@ -27,7 +27,10 @@ class UNet3D(nn.Module):
         self.decoder1 = UBlock(features[1] + features[0], features[0], features[0])
 
         # Upsample, downsample and output steps
-        self.upsample = nn.Upsample(scale_factor=2, mode="trilinear", align_corners=True)
+        # self.upsample = nn.Upsample(scale_factor=2, mode="trilinear", align_corners=True)
+        self.upsample3 = nn.ConvTranspose3d(in_channels=features[3], out_channels=features[3], kernel_size=2, stride=2)
+        self.upsample2 = nn.ConvTranspose3d(in_channels=features[2], out_channels=features[2], kernel_size=2, stride=2)
+        self.upsample1 = nn.ConvTranspose3d(in_channels=features[1], out_channels=features[1], kernel_size=2, stride=2)
         self.downsample = nn.MaxPool3d(2, 2)
         self.output = conv1x1(features[0], regions)
 
@@ -45,11 +48,11 @@ class UNet3D(nn.Module):
         bottleneck = self.bottleneck(p3)
 
         # Decoder
-        up3 = self.upsample(bottleneck)
+        up3 = self.upsample3(bottleneck)
         d3 = self.decoder3(torch.cat([e3, up3], dim=1))
-        up2 = self.upsample(d3)
+        up2 = self.upsample2(d3)
         d2 = self.decoder2(torch.cat([e2, up2], dim=1))
-        up1 = self.upsample(d2)
+        up1 = self.upsample1(d2)
         d1 = self.decoder1(torch.cat([e1, up1], dim=1))
 
         # Output
@@ -58,19 +61,25 @@ class UNet3D(nn.Module):
         return out
 
 
-
-def test():
+if __name__ == "__main__":
     seq_input = torch.rand(1, 4, 160, 224, 160)
     seq_ouput = torch.rand(1, 3, 160, 224, 160)
 
     model = UNet3D(sequences=4, regions=3)
     preds = model(seq_input)
+    # from src.utils.models import count_parameters
+    # print(count_parameters(model))
+    param_size = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    buffer_size = 0
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
+
+    size_all_mb = (param_size + buffer_size) / 1024 ** 2
+    print('model size: {:.3f}MB'.format(size_all_mb))
 
     print(seq_input.shape)
     print(preds.shape)
 
     assert seq_ouput.shape == preds.shape
-
-
-if __name__ == "__main__":
-    test()
